02857b11ed732b9e8a9a4919339424e864c3326f
Izabela Nowicka <izabela.nowicka@riscosoftware.pl>
2018-04-24 13:34:29 +0200

    change database

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('event', 'rw_dev', 'rw_dev', {
-    host: 'localhost',
+module.exports = new Sequelize('dfdrfd7hdch1gp', 'brqxsmzmquajsq', '22f3897b68404c9b5670b2501f8aa2b3f07cf2483818e6a26b4aabe65785a52d', {
+    host: 'ec2-107-20-151-189.compute-1.amazonaws.com',
     dialect: 'postgres',
     logging: true,
-    benchmark: true
+    benchmark: true,
+    port: 5432
 });
